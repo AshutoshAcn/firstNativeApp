3b2d3d3b2dace24fab2b2a566371cc32113675e4
AshutoshAcn <Ashutosh.Lakshakar@acnhealthcare.com>
2024-11-17 12:09:53 +0530

    feat tab setup done with icons

---
+++ my-app/app/(tabs)/explore.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Image, Platform,Text,View } from 'react-native';
+
+
+export default function TabTwoScreen() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <Text>Explore Page</Text>
+    </View>
+  );
+}
+
